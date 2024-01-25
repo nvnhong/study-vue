ec07dcdd0d14c2c40fb54dfd2a067667a0939105
hongmikyeong <mikyeong.dev@gmail.com>
2024-01-25 10:04:05 +0900

    튜토리얼2: 속성 바인딩

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,36 @@
-<script setup>
+<!-- <script setup>
 import { ref } from "vue";
-import { reactive } from "vue";
+const titleClass = ref("title");
+</script> -->
 
-// script에서 ref에 접근할 때 .value를 사용한다.
-const message1 = ref("안녕 Vue!");
-console.log(message1.value);
-message1.value = "메세지 변경됨";
-console.log(message1.value);
-
-const message2 = ref("Hello World");
-const counter = reactive({ count: 10 });
+<script>
+export default {
+  data() {
+    return {
+      titleClass: "title",
+    };
+  },
+};
 </script>
 
 <template>
-  <p>message1 : {{ message1 }}</p>
-  <p>message2 : {{ message2 }}</p>
-  <p>count : {{ counter.count }}</p>
+  <h1 :class="titleClass">나를 빨갛게 만들어보세요.</h1>
 </template>
 
+<style>
+.title {
+  color: red;
+}
+</style>
+
 <!-- 
-[선언적 렌더링]
+[속성 바인딩]
+Vue에서 이중 중괄호는 텍스트 삽입에만 사용됩니다.
+속성을 동적 값에 바인딩하려면 v-bind 디렉티브를 사용합니다.
 
-SFC(Single File Component)는 HTML, CSS, Javascript를 캡슐화한 코드 블록으로 재사용 가능한 .vue 파일입니다.
+<div v-bind:id="dynamicId"></div>
 
-Vue의 핵심 기능은 선언적 렌더링입니다. 
-HTML을 확장하는 템플릿 문법을 사용하여 JavaScript 상태를 기반으로 HTML이 어떻게 보이는지 설명할 수 있습니다. 
-상태가 변경되면 HTML이 자동으로 업데이트 됩니다.
+디렉티브는 v- 접두사로 시작하는 특수한 속성으로 Vue 템플릿 문법의 일부입니다.
+텍스트 삽입과 유사하게 디렉티브 값은 컴포넌트의 상태에 접근할 수 있는 JavaScript 표현식입니다.
 
-변경 시, 업데이트를 트리거할 수 있는 상태는 반응형으로 간주됩니다.
-Vue의 reactive() API를 사용하여 반응형 상태를 선언할 수 있습니다.
-reactive()로 생성된 객체는 일반 객체처럼 작동하는 JavaScript 프락시입니다.
-
-reactive()는 객체(배열, Map, Set과 같은 빌트인 타입 포함)에서만 작동합니다.
-반면에 ref()는 모든 타입의 값을 사용할 수 있으며, .value 속성으로 내부 값을 노출하는 객체를 생성합니다.
-
-컴포넌트의 <script setup> 블록에 선언된 반응형 상태는 템플릿에서 직접 사용할 수 있습니다.
-템플릿에서 선언된 ref 값에 접근할 때 .value를 사용할 필요가 없습니다.
-보다 간결한 사용을 위해 자동으로 언래핑됩니다.
  -->
