8cc42df8d2120cb2502e342c158320fe0b509028
hongmikyeong <mikyeong.dev@gmail.com>
2024-01-25 17:36:08 +0900

    튜토리얼8: 생명주기와 템플릿 참조

--- src/App.vue
+++ src/App.vue
@@ -1,118 +1,63 @@
-<!-- Option -->
-<script>
-let id = 0;
-
+<!-- < Options > -->
+<!-- <script>
 export default {
-  data() {
-    return {
-      newTodo: "",
-      hideCompleted: false,
-      todos: [
-        { id: id++, text: "HTML 배우기", done: true },
-        { id: id++, text: "JavaScript 배우기", done: true },
-        { id: id++, text: "Vue 배우기", done: false },
-      ],
-    };
-  },
-  computed: {
-    filteredTodos() {
-      return this.hideCompleted
-        ? this.todos.filter((t) => !t.done)
-        : this.todos;
-    },
-  },
-  methods: {
-    addTodo() {
-      this.todos.push({ id: id++, text: this.newTodo, done: false });
-      this.newTodo = "";
-    },
-    removeTodo(todo) {
-      this.todos = this.todos.filter((t) => t !== todo);
-    },
+  mounted() {
+    // 이제 컴포넌트가 마운트 되었습니다.
+    this.$refs.pElementRef.textContent = "생명주기와 템플릿 참조";
   },
 };
-</script>
-
-<!-- < Composition > -->
-<!-- <script setup>
-import { ref, computed } from "vue";
-
-let id = 0;
-
-const newTodo = ref("");
-const hideCompleted = ref(false);
-const todos = ref([
-  { id: id++, text: "HTML 배우기", done: true },
-  { id: id++, text: "JavaScript 배우기", done: true },
-  { id: id++, text: "Vue 배우기", done: false },
-]);
-
-function addTodo() {
-  newTodo.value.push({ id: id++, text: newTodo.value, done: false });
-  newTodo.value = "";
-}
-
-function removeTodo(todo) {
-  todos.value = todos.value.filter((t) => t !== todo);
-}
-
-const filteredTodos = computed(() => {
-  return hideCompleted.value ? todos.value.filter((t) => !t.done) : todos.value;
-});
 </script> -->
 
+<!-- < Composition > -->
+<script setup>
+import { ref, onMounted } from "vue";
+
+const pElementRef = ref(null);
+
+onMounted(() => {
+  // 이제 컴포넌트가 마운트 되었습니다.
+  pElementRef.value.textContent = "생명주기와 템플릿 참조";
+});
+</script>
+
 <template>
-  <form @submit,prevent="addTodo">
-    <input v-model="newTodo" />
-    <button>Add Todo</button>
-  </form>
-
-  <ul>
-    <li v-for="todo in filteredTodos" :key="todo.id">
-      <input type="checkbox" v-model="todo.done" />
-      <span :class="{ done: todo.done }">{{ todo.text }}</span>
-      <button @click="removeTodo(todo)">Delete</button>
-    </li>
-  </ul>
-
-  <button @click="hideCompleted = !hideCompleted">
-    {{ hideCompleted ? "Show all" : "Hide Completed" }}
-  </button>
+  <p ref="pElementRef">안녕</p>
 </template>
 
-<style>
-.done {
-  text-decoration: line-through;
-}
-</style>
-
 <!-- 
-[계산된 속성]
-이전 단계의 할 일 목록을 계속 만들어 나가봅시다.
-여기에 이미 각 할 일에 토글 기능을 추가했습니다.
-이것은 각 할 일 객체에 done 속성을 추가하고, v-model을 사용하여 체크박스에 바인딩함으로써 작동합니다.
+[생명주기와 템플릿 참조]
+지금까지 Vue는 반응형 및 선언적 렌더링으로 모든 DOM 업데이트를 처리해 왔습니다.
+그러나 필연적으로 DOM을 수동으로 작업해야 하는 경우가 있습니다.
 
-<li v-for="todo in todos">
-  <input type="checkbox" v-model="todo.done"/>
-</li>
+우리는 특별한 속성인 ref를 사용하여 템플릿 참조를 요청할 수 있습니다.
+<p ref="pElementRef">안녕</p>
 
-다음 개선 사항은 이미 완료된 할 일을 숨길 수 있는 기능을 추가하는 것입니다.
-이미 hideCompleted 상태를 토글하는 버튼이 있습니다.
-하지만 그 상태를 바탕으로 목록 내 항목을 어떻게 렌더링해야 할까요?
+1. Option 에서 
+엘리먼트는 this.$refs에 this.$refs.pElementRef 로 노출됩니다.
+그러나 컴포넌트가 마운트된 후에만 접근할 수 있습니다.
 
-computed()를 소개합니다.
-(Composition)반응 데이터 소스를 기반으로 .value를 계산하는 계산된 참조(ref)를 만들 수 있습니다. 
-(Option) computed 옵션을 사용하여 반응적으로 계산 되는 속성을 선언할 수 있습니다.
+마운트된 후, 코드를 실행하려면 mounted 옵션을 사용해야 합니다.
+export default {
+  mounted() {
+    // 이제 컴포넌트가 마운트 되었습니다.
+  }
+}
 
-import {ref, computed} from 'vue'
+2. Composition 에서
+참조에 접근하려면 다음과 같이 이름이 일치하는 ref를 선언해야 합니다.
+const pElementRef = ref(null);
 
-const hideCompleted = ref(false);
-const todos = ref([/* ... */])
+ref는 null 값으로 초기화합니다.
+<script setup> 실행 시 해당 엘리먼트가 아직 존재하지 않기 때문입니다.
+템플릿 참조는 컴포넌트가 마운트된 후에만 접근할 수 있습니다.
 
-const filterTodos = computed(() => {
-  // todos.value 및 hideCompleted.value를 기반으로 필터링된 할 일을 반환.
+마운트 된 후, 코드를 실행하려면 onMounted() 함수를 사용해야 합니다.
+import { onMounted } from 'vue'
+onMounted(() => {
+  // 이제 컴포넌트가 마운트 되었습니다.
 })
 
-계산된 속성은 계산에 사용된 다른 반응형 상태를 의존성으로 추적합니다.
-결과를 캐시하고 의존성이 변경되면 자동으로 업데이트합니다.
+이것을 생명 주기 훅이라고 하며, 생명 주기의 특정 시간에 호출할 콜백을 등록할 수 있습니다.
+created 및 updated와 같은 다른 훅도 있습니다.
+참고 : https://ko.vuejs.org/guide/essentials/lifecycle#lifecycle-diagram
  -->
