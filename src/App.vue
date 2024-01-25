4c5524b25bda24fd7fde38bb0889709080c2f35f
hongmikyeong <mikyeong.dev@gmail.com>
2024-01-25 17:18:56 +0900

    튜토리얼7: 계산된 속성

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,118 @@
-<!-- Options -->
+<!-- Option -->
 <script>
-// 각 할 일에 고유한 id 부여
 let id = 0;
 
 export default {
   data() {
     return {
-      id: 0,
       newTodo: "",
+      hideCompleted: false,
       todos: [
-        { id: id++, text: "HTML 배우기" },
-        { id: id++, text: "CSS 배우기" },
-        { id: id++, text: "JavaScript 배우기" },
+        { id: id++, text: "HTML 배우기", done: true },
+        { id: id++, text: "JavaScript 배우기", done: true },
+        { id: id++, text: "Vue 배우기", done: false },
       ],
     };
   },
+  computed: {
+    filteredTodos() {
+      return this.hideCompleted
+        ? this.todos.filter((t) => !t.done)
+        : this.todos;
+    },
+  },
   methods: {
     addTodo() {
-      this.todos.push({ id: id++, text: this.newTodo });
+      this.todos.push({ id: id++, text: this.newTodo, done: false });
       this.newTodo = "";
     },
     removeTodo(todo) {
-      this.todos = this.todos.filter((value) => todo !== value);
+      this.todos = this.todos.filter((t) => t !== todo);
     },
   },
 };
 </script>
 
-<!-- Composition -->
+<!-- < Composition > -->
 <!-- <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 
-// 각 할 일에 고유한 id 부여
 let id = 0;
 
 const newTodo = ref("");
+const hideCompleted = ref(false);
 const todos = ref([
-  { id: id++, text: "HTML 배우기" },
-  { id: id++, text: "CSS 배우기" },
-  { id: id++, text: "JavaScript 배우기" },
+  { id: id++, text: "HTML 배우기", done: true },
+  { id: id++, text: "JavaScript 배우기", done: true },
+  { id: id++, text: "Vue 배우기", done: false },
 ]);
 
 function addTodo() {
-  todos.value.push({ id: id++, text: newTodo.value });
+  newTodo.value.push({ id: id++, text: newTodo.value, done: false });
   newTodo.value = "";
 }
 
 function removeTodo(todo) {
-  todos.value = todos.value.filter((value) => todo !== value);
+  todos.value = todos.value.filter((t) => t !== todo);
 }
+
+const filteredTodos = computed(() => {
+  return hideCompleted.value ? todos.value.filter((t) => !t.done) : todos.value;
+});
 </script> -->
 
 <template>
-  <form @submit.prevent="addTodo">
+  <form @submit,prevent="addTodo">
     <input v-model="newTodo" />
-    <button>할 일 추가</button>
+    <button>Add Todo</button>
   </form>
 
   <ul>
-    <li v-for="todo in todos" :key="todo.id">
-      {{ todo.text }}
-      <button @click="removeTodo(todo)">삭제</button>
+    <li v-for="todo in filteredTodos" :key="todo.id">
+      <input type="checkbox" v-model="todo.done" />
+      <span :class="{ done: todo.done }">{{ todo.text }}</span>
+      <button @click="removeTodo(todo)">Delete</button>
     </li>
   </ul>
+
+  <button @click="hideCompleted = !hideCompleted">
+    {{ hideCompleted ? "Show all" : "Hide Completed" }}
+  </button>
 </template>
 
+<style>
+.done {
+  text-decoration: line-through;
+}
+</style>
+
 <!-- 
-[ 리스트 렌더링 ]
+[계산된 속성]
+이전 단계의 할 일 목록을 계속 만들어 나가봅시다.
+여기에 이미 각 할 일에 토글 기능을 추가했습니다.
+이것은 각 할 일 객체에 done 속성을 추가하고, v-model을 사용하여 체크박스에 바인딩함으로써 작동합니다.
 
-v-for 디렉티브를 사용하여 자료 배열을 엘리먼트 목록으로 렌더링할 수 있습니다.
-<ul>
-  <li v-for="todo in todos" :key="todo.id">{{ todo.text }}</li>
-</ul>
+<li v-for="todo in todos">
+  <input type="checkbox" v-model="todo.done"/>
+</li>
 
-여기서 todo는 현재 반복 중인 배열 엘리먼트를 나타내는 로컬 변수입니다.
-함수 범위와 유사하게 v-for 엘리먼트 위 또는 내부에서만 액세스할 수 있습니다.
+다음 개선 사항은 이미 완료된 할 일을 숨길 수 있는 기능을 추가하는 것입니다.
+이미 hideCompleted 상태를 토글하는 버튼이 있습니다.
+하지만 그 상태를 바탕으로 목록 내 항목을 어떻게 렌더링해야 할까요?
 
-각 todo 객체에 고유한 id를 부여하고, 각 <li>에 특별한 속성인 key를 바인딩했습니다.
-key를 사용하면 Vue가 각 <li>를 정확하게 이동시켜 배열에서 해당 객체의 위치와 일치하도록 할 수 있습니다.
+computed()를 소개합니다.
+(Composition)반응 데이터 소스를 기반으로 .value를 계산하는 계산된 참조(ref)를 만들 수 있습니다. 
+(Option) computed 옵션을 사용하여 반응적으로 계산 되는 속성을 선언할 수 있습니다.
+
+import {ref, computed} from 'vue'
+
+const hideCompleted = ref(false);
+const todos = ref([/* ... */])
+
+const filterTodos = computed(() => {
+  // todos.value 및 hideCompleted.value를 기반으로 필터링된 할 일을 반환.
+})
+
+계산된 속성은 계산에 사용된 다른 반응형 상태를 의존성으로 추적합니다.
+결과를 캐시하고 의존성이 변경되면 자동으로 업데이트합니다.
  -->
