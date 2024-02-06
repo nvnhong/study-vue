8eb19c7262d3f777970d4cb3a7080baccea7b5db
mikyeong <mikyeong.dev@gmail.com>
2024-02-06 14:37:36 +0900

    예제4: 조건문과 반복문

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,20 @@
-<!--
-Here we are reactively binding element attributes / properties to the state.
-The :title syntax is short for v-bind:title.
--->
-
 <template>
-  <p>
-    <span :title="message">
-      Hover your mouse over me for a few seconds to see my dynamically bound title!
-    </span>
-  </p>
-  
-  <!--
-  class bindings have special support for objects and arrays
-  in addition to plain strings
-  -->
-  <p :class="{red: isRed}" @click="toggleRed">
-    This should be red... but click me to toggle it.
-  </p>
+  <button @click="show = !show">Toggle List</button>
+  <button @click="list.push(list.length + 1)">Push Number</button>
+  <button @click="list.pop()">Pop Nubmer</button>
+  <button @click="list.reverse()">Reverse List</button>
 
-  <!--
-  style bindings also support object and arrays
-  여기서 중괄호 {}를 사용하는 것은 객체 리터럴을 생성하는데,
-  객체의 키는 스타일 속성이 되고, 값은 해당 스타일 속성에 바인딩할 데이터 변수가 됩니다
-  -->
-  <p :style="{color}" @click="toggleColor">
-    This should be green, and should toggle between green and blue on click.
-  </p>
+  <ul v-if="show && list.length">
+    <li v-for="item of list">{{ item }}</li>
+  </ul>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      message: 'Hello World!',
-      isRed: true,
-      color: 'green'
-    }
-  },
-  methods: {
-    toggleRed() {
-      this.isRed = !this.isRed
-    },
-    toggleColor() {
-      this.color = this.color === 'green' ? 'blue' : 'green'
+      show: true,
+      list: [1, 2, 3]
     }
   }
 }
@@ -51,21 +23,6 @@ export default {
 <!--<script setup>-->
 <!--import {ref} from "vue";-->
 
-<!--const message = ref('Hello World!');-->
-<!--const isRed = ref(true);-->
-<!--const color = ref('green')-->
-
-<!--function toggleRed() {-->
-<!--  isRed.value = !isRed.value;-->
-<!--}-->
-
-<!--function toggleColor() {-->
-<!--  color.value = color.value === 'green' ? 'blue' : 'green'-->
-<!--}-->
+<!--const show = ref(true);-->
+<!--const list = ref([1, 2, 3])-->
 <!--</script>-->
-
-<style>
-.red {
-  color: red;
-}
-</style>
